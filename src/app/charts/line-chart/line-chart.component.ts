c4b1fcbc05cf05a703794d3ec1a5771396e76bdb
jardmclan <mcleanj@hawaii.edu>
2019-08-01 16:25:36 -1000

    hidden 100%

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -98,6 +98,7 @@ export class LineChartComponent implements OnInit {
 
   createBarChart(labels: any[], data: any[]) {
     this.ctx = this.chartDiv.nativeElement.getContext('2d');
+    let ctx = this.ctx;
     this.myChart = new Chart(this.ctx, {
       type: "bar",
       data: {
@@ -158,9 +159,67 @@ export class LineChartComponent implements OnInit {
               labelString: 'Recharge (Million Gallons Per Day)'
             }
           }]
+        },
+        animation: {
+          onProgress: function (animation) {
+            //console.log(animation);
+            let chartInstance = this.chart;
+            ctx = chartInstance.ctx;
+            ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'bottom';
+
+            this.data.datasets.forEach(function (dataset, i) {
+              let meta = chartInstance.controller.getDatasetMeta(i);
+              meta.data.forEach(function (bar, index) {
+                let data = dataset.data[index];
+                ctx.fillStyle = "rgba(0, 0, 0, 1)";
+                //console.log(bar);
+                let width = bar._model.width;
+                let height = Chart.defaults.global.defaultFontSize + 5;
+                let x = bar._model.x;
+                let y = bar._model.y;
+                ctx.fillRect(x - width / 2, y - height, width, height);
+                ctx.fillStyle = "rgba(255, 255, 255, 1)";                         
+                ctx.fillText(data, bar._model.x, bar._model.y - 2);
+              });
+            });
+          }
         }
       }
+      
     });
+
+    console.log(this.myChart.options.animation.onProgress);
+
+    // setTimeout(() => {
+    //   ctx = this.myChart.ctx;
+    //   ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+    //   ctx.textAlign = 'center';
+    //   ctx.textBaseline = 'bottom';
+    //   ctx.fillStyle = "rgba(255, 255, 255, 1)";
+  
+    //   //for(let i = 0; i < data.length; i++) {
+    //   console.log("1");
+    //   let meta = this.myChart.controller.getDatasetMeta(0);
+    //   console.log("2");
+    //   meta.data.forEach((bar, index) => {
+    //     let value = data[index];
+    //     console.log(value, bar);
+    //     ctx.fillText(value, bar._model.x, bar._model.y - 5);
+    //   });
+    // }, 5000);
+   
+    //}
+    // data.forEach(function (dataset, i) {
+    //   console.log("1");
+    //   let meta = this.myChart.controller.getDatasetMeta(i);
+    //   console.log("2");
+    //   meta.data.forEach(function (bar, index) {
+    //     let data = dataset.data[index];                            
+    //     ctx.fillText(data, bar._model.x, bar._model.y - 5);
+    //   });
+    // });
 
     // this.ctx.textAlign = 'center';
     // this.ctx.fillStyle = "rgba(255, 255, 255, 1)";
